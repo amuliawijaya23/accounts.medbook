4758ba720aacb7405eb9d0d1f041d1ad4f5955d5
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-15 18:06:26 +0700

    establishes connection to mongo db

--- src/index.js
+++ src/index.js
@@ -5,9 +5,26 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const http = require('http');
 
-const { PORT } = process.env;
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
+
+if (!MONGODB_URL || MONGODB_URL.length === 0) {
+  throw new Error('Please add your MongoDB URL');
+}
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on('error', (error) => {
+  throw new Error(error);
+});
+mongoose.connection.once('connected', () => {
+  console.log('New DB connection established');
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
