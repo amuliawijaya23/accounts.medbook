109fcb6202a888e94365ba79571a717db2114a4b
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-17 00:27:23 +0700

    adds oauth2 configurations

--- src/db/models/RefreshToken.js
+++ src/db/models/RefreshToken.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 
 const refreshTokenSchema = new mongoose.Schema({
   token: { type: String, required: true },
-  clientId: { type: String, required: true, ref: 'Client' },
-  userId: { type: String, required: true, ref: 'User' },
-  scope: { type: String, required: true },
 });
 
 exports.RefreshToken = mongoose.model.RefreshToken || mongoose.model('RefreshToken', refreshTokenSchema);
--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,9 +1,7 @@
 const passport = require('passport');
 
-const { isNotAuthenticated } = require('../middleware');
-
 module.exports = (router) => {
-  router.post('/login', isNotAuthenticated, passport.authenticate('local', { session: true }), (req, res) => res.status(200).json(req.user).end());
+  router.post('/login', passport.authenticate('local', { session: true }), (req, res) => res.status(200).json(req.user).end());
   router.delete('/logout', async (req, res, next) => {
     await req.logout((error) => {
       if (error) {
