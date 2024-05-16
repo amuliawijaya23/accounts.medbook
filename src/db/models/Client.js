476a258f5361d2de05d3eba0859924f89e78768d
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-16 20:58:34 +0700

    creates createClient function

--- src/db/models/Client.js
+++ src/db/models/Client.js
@@ -9,3 +9,4 @@ const clientSchema = new mongoose.Schema({
 exports.Client = mongoose.model.Client || mongoose.model('Client', clientSchema);
 exports.getClientByClientId = (clientId) => this.Client.findOne({ clientId });
 exports.getClientById = (id) => this.Client.findById(id);
+exports.createClient = (values) => new this.Client(values).save().then((client) => client.toObject());
